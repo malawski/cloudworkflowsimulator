eb106ad9a82b0bcbab8d018e7c9f878abf2d8e05
malawski <malawski@agh.edu.pl>
2011-10-21 17:25:38 -0400

    Added a simple datacenter implementation and a test client.

--- src/cws/core/WorkflowEvent.java
+++ src/cws/core/WorkflowEvent.java
@@ -19,4 +19,25 @@ public interface WorkflowEvent {
     
     /** Transfer is complete */
     public static final int TRANSFER_COMPLETE = 4;
+    
+    /////////////////////////////////////////////////////////
+    // VM EVENTS
+    /////////////////////////////////////////////////////////
+    
+    /** Client entity submits a new VM creation request */
+    public static final int NEW_VM = 5;
+    
+    /** VM creation is complete */
+    public static final int VM_CREATION_COMPLETE = 6;
+    
+    /** Client entity submits a new cloudlet to a VM */
+    public static final int CLOUDLET_SUBMIT = 7;
+
+    /** Cloudlet execution is started */
+    public static final int CLOUDLET_STARTED = 8;
+
+    /** Cloudlet execution is comlplete */
+    public static final int CLOUDLET_COMPLETE = 9;
+
+    
 }
