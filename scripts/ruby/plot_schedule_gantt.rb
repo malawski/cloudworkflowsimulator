991107e3906609a5573143e1189260cf51397b48
malawski <malawski@agh.edu.pl>
2011-10-28 15:21:18 -0400

    Test many workflows with priorities

--- scripts/ruby/plot_schedule_gantt.rb
+++ scripts/ruby/plot_schedule_gantt.rb
@@ -1,6 +1,7 @@
 require 'scanf.rb'
 require 'rubygems'
 require 'gnuplot'
+require 'set'
 
 
 # Plot Gantt charts from CloudSim log files using Gnuplot
@@ -8,28 +9,45 @@ require 'gnuplot'
 # Author: Maciej Malawski
 
 
+class Tasks
+  attr_reader :xlo, :xhi, :y, :ids, :types, :distinct_types
+  
+  def read_tasks (filename)
+    
+    str = `cat #{filename}.txt | grep SUCCESS`
+
+    jobs = Hash.new
+    @distinct_types = Set.new
+
+    str.each do |line|
+      data = line.scanf(" %d   SUCCESS  %d %d %f %f %f")
+      jobs[data[0]] = [data [1], data[2], data[4], data[5]]
+    end
+
+    @xlo = Array.new
+    @xhi = Array.new
+    @y = Array.new
+    @ids = Array.new
+    @types = Array.new
+
+    jobs.keys.each do |id|
+      @xlo[id] = jobs[id][2]
+      @xhi[id] = jobs[id][3]
+      @y[id] = jobs[id][1]
+      @ids[id] = id
+      @types[id] = jobs[id][0]
+      @distinct_types.add(jobs[id][0])
+    end
+  end
+  
+  
+end
+
+
 def plot_schedule (filename)
 
-  str = `cat #{filename}.txt | grep SUCCESS`
-
-  cloudlets = Hash.new
-
-  str.each do |line|
-    data = line.scanf(" %d   SUCCESS  %d %d %f %f %f")
-    cloudlets[data[0]] = [data [1], data[2], data[4], data[5]]
-  end
-
-  xlo = Array.new
-  xhi = Array.new
-  y = Array.new
-  ids = Array.new
-
-  cloudlets.keys.each do |id|
-    xlo[id] = cloudlets[id][2]
-    xhi[id] = cloudlets[id][3]
-    y[id] = cloudlets[id][1]
-    ids[id] = id
-  end
+  tasks = Tasks.new
+  tasks.read_tasks(filename)
 
   str = `cat #{filename}-vms.txt | grep -e '[0-9]'  `
 
@@ -54,7 +72,7 @@ def plot_schedule (filename)
 
   ymax = vmy.length + 0.8
   
-  puts "Saving plot to file: " + filename + ".pdf"
+  #puts "Saving plot to file: " + filename + ".pdf"
 
   Gnuplot.open do |gp|
     Gnuplot::Plot.new( gp ) do |plot|
@@ -62,24 +80,33 @@ def plot_schedule (filename)
       plot.title  "Schedule " + File.basename(filename)
       plot.xlabel "time"
       plot.ylabel "VM ID"
-      plot.ytics 50
+      #plot.ytics 50
       #plot.yrange "[-0.8:#{ymax}]"
-      plot.terminal 'pdf size 8.5,11 font "arial,6" linewidth 1'
-      plot.output filename + ".pdf"
+      #plot.terminal 'pdf size 8.5,11 font "arial,6" linewidth 1'
+      #plot.output filename + ".pdf"
 
-      plot.data = [
-        Gnuplot::DataSet.new( [vmxlo, vmy, vmxlo, vmxhi] ) do |ds|
+      data = Array.new
+      
+      vmset = Gnuplot::DataSet.new( [vmxlo, vmy, vmxlo, vmxhi] ) do |ds|
         ds.using = "1:2:3:4:($2-0.5):($2+0.5)"
         ds.with = "boxxyerrorbars fs solid 0.55 noborder"
         ds.title = 'VMs'
-        end,
-
-        Gnuplot::DataSet.new( [xlo, y, xlo, xhi] ) do |ds|
-        ds.using = "1:2:3:4:($2-0.4):($2+0.4)"
-        ds.with = "boxxyerrorbars fs solid 0.55 noborder"
-        ds.title = 'tasks'
+      end
+      
+      data.push(vmset)
+      
+      tasks.distinct_types.to_a.sort.each do |type|
+        # here we do filtering based on type (e.g. priority)
+        dataset = Gnuplot::DataSet.new( [tasks.xlo, tasks.y, tasks.xlo, tasks.xhi, tasks.types] ) do |ds|
+          ds.using = "($5==#{type} ? $1 : NaN):2:3:4:($2-0.4):($2+0.4)"
+          ds.with = "boxxyerrorbars fs solid 0.55"
+          ds.title = "#{type}"
         end
-      ]
+        data.push(dataset)
+      end
+
+                    
+      plot.data = data
       
     end
 
@@ -99,4 +126,13 @@ end
 #plot_schedule 'testDatacenterDeprovisioner100DAG'
 #plot_schedule 'output/testDatacenterDeprovisioner1000DAG'
 #plot_schedule 'output/DeprovisionerCyberShake_100'
-plot_schedule 'output/Deprovisionercybershake_small'
+#plot_schedule 'output/Deprovisionercybershake_small'
+#plot_schedule 'output/EnsembleCyberShake_30_50'
+#plot_schedule 'output/EnsembleCyberShake_10x100'
+#plot_schedule 'output/CyberShake_100.dag10x20'
+#plot_schedule 'output/CyberShake_100.dag20x10'
+#plot_schedule 'output/CyberShake_100.dag20x9'
+#plot_schedule 'output/CyberShake_1000.dag20x10'
+plot_schedule 'output/CyberShake_1000.dag200x10'
+#plot_schedule 'output/cybershake_small.dag256x10'
+#plot_schedule 'output/cybershake_small.dag2560x10'
--- src/cws/core/datacenter/CloudTest.java
+++ src/cws/core/datacenter/CloudTest.java
@@ -9,10 +9,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.text.DecimalFormat;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Set;
 
 import org.cloudbus.cloudsim.Log;
@@ -36,6 +40,117 @@ public class CloudTest {
 	
 	private static String OUTPUT_PATH = "output";
 
+	
+	
+	/**
+	 * This cloud client schedules multiple DAGs
+	 * on statically provisioned set of VMs.
+	 * Each DAG has a priority assigned.
+	 * Client keeps a priority queue of eligible jobs
+	 * from all the workflows and submits them to free VMs.
+	 *
+	 * @author malawski
+	 *
+	 */
+	private class CloudEnsembleDAGClient extends CloudDAGClient {
+		
+
+		// each job has a priority
+		private Map<Job, Integer> jobPriorities;
+		// each job is assigned to a workflow
+		private Map<Job,Workflow> workflows;
+		
+		
+		/**
+		 * 
+		 * Tasks IDs are not globally unique, so we need to group them into workflows.
+		 * This class provides mapping from tasks to jobs.
+		 *
+		 */
+		private class Workflow {
+			
+			protected Map<Task,Job> tasks2jobs;
+			
+			public Workflow() {
+				tasks2jobs = new HashMap<Task, Job>();
+			}
+			
+			public void add(Task task, Job job) {
+				tasks2jobs.put(task, job);
+			}
+			
+			public Job get(Task task) {
+				return tasks2jobs.get(task);
+			}
+			
+		}
+		
+		/**
+		 * Compares jobs based on their priority
+		 */
+		private class JobComparator implements Comparator<Job> {
+
+			@Override
+			public int compare(Job o1, Job o2) {
+				return jobPriorities.get(o1) - jobPriorities.get(o2);
+			}
+			
+		}
+		
+
+		public CloudEnsembleDAGClient(String name) {
+			super(name);
+			jobPriorities = new HashMap<Job, Integer>();
+			
+			// here we use a priority queue instead of FIFO queue
+			eligibleJobs = new PriorityQueue<Job>(64, new JobComparator());
+			
+			workflows = new HashMap<Job, Workflow>();
+		}
+		
+		public Map<Job, Integer> getJobPriorities() {
+			return jobPriorities;
+		}
+		
+		public void addDAG(DAG dag, int priority) {
+			Workflow workflow = new Workflow();
+			String[] tasks = dag.getTasks();
+			for (int i = 0; i< tasks.length; i++) {
+				Task task = dag.getTask(tasks[i]);
+				Job job = createJob(task);
+				workflow.add(task, job);
+				workflows.put(job, workflow);
+				jobPriorities.put(job, priority);
+				if (task.parents.isEmpty())
+					eligibleJobs.add(job);
+			}
+		}	
+		
+		protected void completeJob(Job job) {
+			Log.printLine(CloudSim.clock() + " Job " + job.getID() + " finished on VM " + job.getVM().getId());
+			
+			VM vm = job.getVM();
+	    	busyVMs.remove(vm);
+	    	freeVMs.add(vm);
+	    	runningJobs.remove(job);
+	    	completedJobs.add(job);
+	    	
+	    	// update eligibility
+	    	for (Task child: job.getTask().children) {
+	    		Set<Job> parents = new HashSet<Job>();
+	    		Workflow workflow = workflows.get(job);
+	    		for (Task parent : child.parents) {
+	    			parents.add(workflow.get(parent));
+	    		}
+	    		if (completedJobs.containsAll(parents))	eligibleJobs.add(workflow.get(child));
+	    	}
+	    	
+	    	scheduleJobs();
+		}	
+		
+	}
+	
+	
 	
 	/**
 	 * This cloud client adds functionality of dynamically provisioning 
@@ -94,32 +209,40 @@ public class CloudTest {
 	 */
 	private class CloudDAGClient extends CloudClient {
 
-		protected Set<Job> eligibleJobs;
+		protected Queue<Job> eligibleJobs;
 		protected Map<Task,Job> tasks2jobs;
 		
-		protected DAG dag;
 		
 		public CloudDAGClient(String name) {
 			super(name);
-			eligibleJobs = new HashSet<Job>();
+			eligibleJobs = new LinkedList<Job>();
 			tasks2jobs = new HashMap<Task, Job>();
 		}
 
 		public void setDAG(DAG dag) {
-			this.dag = dag;
 			String[] tasks = dag.getTasks();
 			for (int i = 0; i< tasks.length; i++) {
 				Task task = dag.getTask(tasks[i]);
-				// we assume that the execution times in seconds are measured on 1000 MIPS processors 
-				double mi = 1000.0 * task.size ;
-				Job job = new Job((int) mi);
-				job.setTask(task);
-				jobs.add(job);
+				Job job = createJob(task);
 				tasks2jobs.put(task, job);
-				if (task.parents.isEmpty()) eligibleJobs.add(job);
+				if (task.parents.isEmpty())
+					eligibleJobs.add(job);
 			}
 		}
 		
+		/**
+		 * Job creation
+		 * Create job object based on a task from DAG
+		 */
+		protected Job createJob(Task task) {
+			// we assume that the execution times in seconds are measured on
+			// 1000 MIPS processors
+			double mi = 1000.0 * task.size;
+			Job job = new Job((int) mi);
+			job.setTask(task);
+			jobs.add(job);
+			return job;
+		}
 		
 		@Override
 		protected void scheduleJobs() {
@@ -129,15 +252,14 @@ public class CloudTest {
 			// if there is nothing to do, just return
 			if (freeVMs.isEmpty()) return;
 			if (newJobs.isEmpty()) return;
-			Iterator<Job> jobIt = eligibleJobs.iterator();
+
 			Iterator<VM> vmIt = freeVMs.iterator();
-			while (jobIt.hasNext() && vmIt.hasNext()) {
-				Job job = jobIt.next();
+			while (!eligibleJobs.isEmpty() && vmIt.hasNext()) {
+				Job job = eligibleJobs.poll(); // retrieve and remove job from eligible set
 				VM vm = vmIt.next();
 				job.setVM(vm);
 				vmIt.remove(); // remove VM from free set
 				busyVMs.add(vm);
-				jobIt.remove(); // remove job from eligible set
 				submittedJobs.add(job);
 				Log.printLine(CloudSim.clock() + " Submitting job " + job.getID() + " to VM " + job.getVM().getId());
 				sendNow(vm.getId(), JOB_SUBMIT, job);				
@@ -387,6 +509,42 @@ public class CloudTest {
 
 	}
 	
+
+	public static void printEnsemble(Set<Job> jobs, Map<Job, Integer> jobPriorities, String fileName) {
+
+		
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw, true);
+		
+		
+		String indent = "    ";
+		pw.println();
+		pw.println("========== OUTPUT ==========");
+		pw.println("Job ID" + indent + "STATUS" + indent
+				+ "Priority" + indent + "VM ID" + indent + "Time" + indent
+				+ "Start Time" + indent + "Finish Time");
+
+		DecimalFormat dft = new DecimalFormat("###.##");
+		for (Job job : jobs) {
+			pw.print(indent + job.getID() + indent + indent);
+
+			if (job.getState() == Job.State.SUCCESS) {
+				pw.print("SUCCESS");
+
+				pw.println(indent + indent + jobPriorities.get(job)
+						+ indent + indent + indent + job.getVM().getId()
+						+ indent + indent
+						+ dft.format(job.getDuration()) + indent
+						+ indent + dft.format(job.getStartTime())
+						+ indent + indent
+						+ dft.format(job.getFinishTime()));
+			}
+		}
+		Log.print(sw.toString());
+		stringToFile(sw.toString(),  fileName + ".txt");
+
+	}
+	
 	
 	public static void printVmList(Set<VM> vms, String name) {
 
@@ -433,8 +591,8 @@ public class CloudTest {
 	
 	
 	
-    @Test
-    public void testDatacenterVMs() {
+    //@Test
+    public void testCloudVMs() {
         CloudSim.init(1, null, false);
         
         CloudClient cloudClient = new CloudClient("Client");
@@ -454,8 +612,8 @@ public class CloudTest {
        
     }
 	
-    @Test
-    public void testDatacenter10VMs() {
+    //@Test
+    public void testCloud10VMs() {
         CloudSim.init(1, null, false);
         
         CloudClient cloudClient = new CloudClient("Client");
@@ -476,8 +634,8 @@ public class CloudTest {
     }
     
 
-	@Test
-	public void testDatacenterJobs() {
+	//@Test
+	public void testCloudJobs() {
 		CloudSim.init(1, null, false);
 
 		CloudClient cloudClient = new CloudClient("Client");
@@ -506,8 +664,8 @@ public class CloudTest {
 
 	}
 
-	@Test
-	public void testDatacenterDAG100() {
+	//@Test
+	public void testCloudDAG100() {
 		
 		CloudSim.init(1, null, false);
 
@@ -521,7 +679,7 @@ public class CloudTest {
 			vms.add(vm);
 		}
 
-		DAG dag = DAGParser.parseDAG(new File("dags/CyberShake_30.dag"));
+		DAG dag = DAGParser.parseDAG(new File("dags/CyberShake_100.dag"));
 		
 		cloudClient.setVMs(vms);
 		cloudClient.setDAG(dag);
@@ -531,7 +689,7 @@ public class CloudTest {
 		assertEquals(vms.size(), cloudClient.getRunningVMs().size());
 		assertEquals(dag.numTasks(), cloudClient.getCompletedJobs().size());
 		
-		printJobs(cloudClient.getCompletedJobs(), "CyberShake_30");
+		printJobs(cloudClient.getCompletedJobs(), "CyberShake_100");
 
 	}
 	
@@ -573,14 +731,188 @@ public class CloudTest {
 	}
 	
 	
-    @Test
+    //@Test
     public void testCloudDeprovisioner100DAG() {
     	runCloudDeprovisioner("dags/CyberShake_100.dag", "DeprovisionerCyberShake_100");        
     }
 
-    @Test
+    //@Test
     public void testCloudDeprovisionerCybershakeDAG() {
     	runCloudDeprovisioner("dags/cybershake_small.dag", "Deprovisionercybershake_small");        
     }
     
+    
+	//@Test
+	public void testEnsemble() {
+		
+		CloudSim.init(1, null, false);
+
+		CloudEnsembleDAGClient cloudClient = new CloudEnsembleDAGClient("Client");
+		Cloud cloud = new Cloud();
+		cloudClient.setCloud(cloud);
+		
+		HashSet<VM> vms = new HashSet<VM>();
+		for (int i = 0; i < 10; i++) {
+			VM vm = new VM(1000, 1, 1.0, 1.0);
+			vms.add(vm);
+		}
+		cloudClient.setVMs(vms);
+		
+		DAG dag;
+		dag = DAGParser.parseDAG(new File("dags/CyberShake_30.dag"));
+		
+
+		int priority = 1;
+		cloudClient.addDAG(dag, priority);
+
+		CloudSim.startSimulation();
+
+		assertEquals(vms.size(), cloudClient.getRunningVMs().size());
+		assertEquals(dag.numTasks(), cloudClient.getCompletedJobs().size());
+		
+		printJobs(cloudClient.getCompletedJobs(), "EnsembleCyberShake_30");
+
+	}
+    
+	@Test
+	public void testEnsemble30_50() {
+		
+		CloudSim.init(1, null, false);
+
+		CloudEnsembleDAGClient cloudClient = new CloudEnsembleDAGClient("Client");
+		Cloud cloud = new Cloud();
+		cloudClient.setCloud(cloud);
+		
+		HashSet<VM> vms = new HashSet<VM>();
+		for (int i = 0; i < 10; i++) {
+			VM vm = new VM(1000, 1, 1.0, 1.0);
+			vms.add(vm);
+		}
+		cloudClient.setVMs(vms);
+		
+		int priority;
+		
+		DAG dag1 = DAGParser.parseDAG(new File("dags/CyberShake_30.dag"));
+		priority = 1;
+		cloudClient.addDAG(dag1, priority);
+
+		DAG dag2 = DAGParser.parseDAG(new File("dags/CyberShake_50.dag"));
+		priority = 2;
+		cloudClient.addDAG(dag2, priority);
+		
+		CloudSim.startSimulation();
+
+		assertEquals(vms.size(), cloudClient.getRunningVMs().size());
+		assertEquals(dag1.numTasks() + dag2.numTasks(), cloudClient.getCompletedJobs().size());
+		
+		printEnsemble(cloudClient.getCompletedJobs(), cloudClient.getJobPriorities(), "EnsembleCyberShake_30_50");
+
+	}
+    
+
+	@Test
+	public void testEnsemble10x100() {
+		
+		CloudSim.init(1, null, false);
+
+		CloudEnsembleDAGClient cloudClient = new CloudEnsembleDAGClient("Client");
+		Cloud cloud = new Cloud();
+		cloudClient.setCloud(cloud);
+		
+		HashSet<VM> vms = new HashSet<VM>();
+		for (int i = 0; i < 10; i++) {
+			VM vm = new VM(1000, 1, 1.0, 1.0);
+			vms.add(vm);
+		}
+		cloudClient.setVMs(vms);
+		
+		int totalTasks = 0;
+		
+		for (int priority =0; priority <10; priority++) {
+			DAG dag = DAGParser.parseDAG(new File("dags/CyberShake_100.dag"));
+			cloudClient.addDAG(dag, priority);
+			totalTasks += dag.numTasks();
+		}
+
+		
+		CloudSim.startSimulation();
+
+		assertEquals(vms.size(), cloudClient.getRunningVMs().size());
+		assertEquals(totalTasks, cloudClient.getCompletedJobs().size());
+		
+		printEnsemble(cloudClient.getCompletedJobs(), cloudClient.getJobPriorities(), "EnsembleCyberShake_10x100");
+
+	}
+	
+	
+	public void runEnsembleMxN(int M, int N, String DAGName) {
+		
+		CloudSim.init(1, null, false);
+
+		CloudEnsembleDAGClient cloudClient = new CloudEnsembleDAGClient("Client");
+		Cloud cloud = new Cloud();
+		cloudClient.setCloud(cloud);
+		
+		HashSet<VM> vms = new HashSet<VM>();
+		for (int i = 0; i < M; i++) {
+			VM vm = new VM(1000, 1, 1.0, 1.0);
+			vms.add(vm);
+		}
+		cloudClient.setVMs(vms);
+		
+		int totalTasks = 0;
+		
+		for (int priority = 0; priority <N; priority++) {
+			DAG dag = DAGParser.parseDAG(new File("dags/" + DAGName));
+			cloudClient.addDAG(dag, priority);
+			totalTasks += dag.numTasks();
+		}
+
+		
+		CloudSim.startSimulation();
+
+		assertEquals(vms.size(), cloudClient.getRunningVMs().size());
+		assertEquals(totalTasks, cloudClient.getCompletedJobs().size());
+		
+		printEnsemble(cloudClient.getCompletedJobs(), cloudClient.getJobPriorities(), DAGName + M + "x" + N);
+
+	}
+	
+	@Test
+	public void testEnsemble10x20x100() {
+		runEnsembleMxN(10, 20, "CyberShake_100.dag");
+	}
+	
+	@Test
+	public void testEnsemble20x10x100() {
+		runEnsembleMxN(20, 10, "CyberShake_100.dag");
+	}
+	
+	@Test
+	public void testEnsemble20x9x100() {
+		runEnsembleMxN(20, 9, "CyberShake_100.dag");
+	}
+
+	@Test
+	public void testEnsemble20x10x1000() {
+		runEnsembleMxN(20, 10, "CyberShake_1000.dag");
+	}
+	
+	@Test
+	public void testEnsemble200x10x1000() {
+		runEnsembleMxN(200, 10, "CyberShake_1000.dag");
+	}
+
+	//@Test
+	public void testEnsemble256x10xsmall() {
+		runEnsembleMxN(256, 10, "cybershake_small.dag");
+	}
+	
+	//@Test
+	public void testEnsemble2560x10xsmall() {
+		runEnsembleMxN(2560, 10, "cybershake_small.dag");
+	}
+
+	
+	
 }
