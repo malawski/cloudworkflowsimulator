fb97c0241fb26d5fa0ec824fe60e8d58c4caec1c
Gideon Juve <juve@usc.edu>
2011-11-03 09:23:42 -0700

    Add initial interface for Provisioner

---
+++ src/cws/core/Provisioner.java
@@ -0,0 +1,10 @@
+package cws.core;
+
+/**
+ * An interface for resource provisioners used by the WorkflowEngine.
+ *
+ * @author Gideon Juve <juve@usc.edu>
+ */
+public interface Provisioner {
+    public void provisionResources(WorkflowEngine engine);
+}
