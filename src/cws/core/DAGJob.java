3e75245b0b71af2a87fcdf1c9f31e28c1221ab24
Gideon Juve <juve@usc.edu>
2011-11-03 09:22:03 -0700

    Make the DAGJob keep track of workflow state

--- src/cws/core/DAGJob.java
+++ src/cws/core/DAGJob.java
@@ -1,9 +1,15 @@
 package cws.core;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
 import cws.core.dag.DAG;
+import cws.core.dag.Task;
 
 /**
- * This class records information about the execution of a DAG.
+ * This class records information about the execution of a DAG, including the
+ * state of all tasks.
  *
  * @author Gideon Juve <juve@usc.edu>
  */
@@ -14,9 +20,29 @@ public class DAGJob {
     /** The DAG being executed */
     private DAG dag;
     
+    /** Set of tasks that have been released */
+    private Set<Task> releasedTasks;
+    
+    /** Set of tasks that are finished */
+    private Set<Task> completedTasks;
+    
+    /** List of all tasks that are ready but have not been claimed */
+    private LinkedList<Task> queue;
+    
     public DAGJob(DAG dag, int owner) {
         this.dag = dag;
         this.owner = owner;
+        this.queue = new LinkedList<Task>();
+        this.releasedTasks = new HashSet<Task>();
+        this.completedTasks = new HashSet<Task>();
+        
+        // Release all root tasks
+        for (String tid : dag.getTasks()) {
+            Task t = dag.getTask(tid);
+            if (t.parents.size() == 0) {
+                releaseTask(t);
+            }
+        }
     }
     
     public int getOwner() {
@@ -33,5 +59,59 @@ public class DAGJob {
     
     public void setDAG(DAG dag) {
         this.dag = dag;
+    }
+    
+    /** Check to see if a task has been released */
+    public boolean isReleased(Task t) {
+        return releasedTasks.contains(t);
+    }
+    
+    /** Check to see if a task has been completed */
+    public boolean isComplete(Task t) {
+        return completedTasks.contains(t);
+    }
+    
+    /** Return true if the workflow is finished */
+    public boolean isFinished() {
+        // The workflow must be finished if all the tasks that
+        // have been released have been completed
+        return releasedTasks.size() == completedTasks.size();
+    }
+    
+    private void releaseTask(Task t) {
+        releasedTasks.add(t);
+        queue.add(t);
+    }
+    
+    /** Mark a task as completed */
+    public void completeTask(Task t) {
+        // Sanity check
+        if (!releasedTasks.contains(t)) {
+            throw new RuntimeException("Task has not been released: "+t);
+        }
+        
+        // Add it to the list of completed tasks
+        completedTasks.add(t);
+        
+        // Release all ready children
+        for (Task c : t.children) {
+            if (!releasedTasks.contains(c)) {
+                if (completedTasks.containsAll(c.parents)) {
+                    releaseTask(c);
+                }
+            }
+        }
+    }
+    
+    /** Return the next ready task */
+    public Task nextReadyTask() {
+        if (queue.size() <= 0)
+            return null;
+        return queue.pop();
+    }
+    
+    /** Return the number of ready tasks */
+    public int readyTasks() {
+        return queue.size();
     }
 }
