cccf1d59dbe1f660c9e41dd5caea2ca4e76db324
Gideon Juve <juve@usc.edu>
2011-11-03 09:23:58 -0700

    Add initial interface for Scheduler

---
+++ src/cws/core/Scheduler.java
@@ -0,0 +1,10 @@
+package cws.core;
+
+/**
+ * An interface for job schedulers used by the WorkflowEngine.
+ *
+ * @author Gideon Juve <juve@usc.edu>
+ */
+public interface Scheduler {
+    public void scheduleJobs(WorkflowEngine engine);
+}
