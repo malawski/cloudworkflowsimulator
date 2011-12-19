e8a1a2bfa823d92f34dbd32495bb925697ce7fab
Gideon Juve <juve@usc.edu>
2011-12-19 13:23:03 -0800

    Clean up critical path algorithm

--- src/cws/core/dag/algorithms/CriticalPath.java
+++ src/cws/core/dag/algorithms/CriticalPath.java
@@ -11,45 +11,54 @@ import cws.core.dag.Task;
  * Compute longest path using topological order, 
  * http://en.wikipedia.org/wiki/Longest_path_problem#Weighted_directed_acyclic_graphs
  * @author malawski
- *
  */
 public class CriticalPath {
-	
-	// earliest start time
-	Map<Task, Double> eft;
-	
-	public CriticalPath(DAG dag, TopologicalOrder order) {
-		
-		eft = new HashMap<Task, Double>();
+    private Map<Task, Double> eft;
+    private Double length = null;
+    
+    public CriticalPath(DAG dag, TopologicalOrder order) {
+        eft = new HashMap<Task, Double>();
 
-		for (Task task : order.order()) {
-			eft.put(task, task.size);
-		}
-		
-		for (Task task : order.order()) {
-			for (Task child : task.children) {
-				eft.put(child, Math.max(eft.get(child), eft.get(task)+child.size));
-			}
-		}		
-	}
-	
-	public double eft(Task task) {
-		return eft.get(task);
-	}
-
-	public double est(Task task) {
-		return eft.get(task) - task.size;
-	}
-	
-	
-	public double getCriticalPathLength() {
-		double len = 0.0;
-		
-		for (Task task : eft.keySet()) {
-			if (eft(task)>len) len=eft(task);
-		}
-		
-		return len;
-	}
-	
+        // Initially the finish time is whatever the runtime is
+        for (Task task : order) {
+            eft.put(task, task.size);
+        }
+        
+        // Now we adjust the values in the topological order
+        for (Task task : order) {
+            for (Task child : task.children) {
+                eft.put(child, Math.max(eft.get(child), eft.get(task)+child.size));
+            }
+        }
+    }
+    
+    /**
+     * @return Earliest finish time of task
+     */
+    public double eft(Task task) {
+        return eft.get(task);
+    }
+    
+    /** 
+     * @return Earliest start time of task
+     */
+    public double est(Task task) {
+        return eft.get(task) - task.size;
+    }
+    
+    /**
+     * @return Length of critical path
+     */
+    public double getCriticalPathLength() {
+        if (length == null) { // Cache
+            double len = 0.0;
+            for (Task task : eft.keySet()) {
+                double eft = eft(task);
+                if (eft > len)
+                    len = eft;
+            }
+            length = len;
+        }
+        return length;
+    }
 }
--- src/cws/core/dag/algorithms/CriticalPathTest.java
+++ src/cws/core/dag/algorithms/CriticalPathTest.java
@@ -11,29 +11,50 @@ import cws.core.dag.DAGParser;
 import cws.core.dag.Task;
 
 public class CriticalPathTest {
-
-	@Test
-	public void testEft() {
-		DAG dag = DAGParser.parseDAG(new File("dags/test.dag"));
+    
+    @Test
+    public void cptest() {
+        DAG dag = DAGParser.parseDAG(new File("dags/cptest.dag"));
         TopologicalOrder order = new TopologicalOrder(dag);
-        for (Task task : order.order()) System.out.print(task + " "); System.out.println();
         CriticalPath cp = new CriticalPath(dag, order);
-        for (Task task : order.order()) {
-        	System.out.println(task + "\t" + task.size + "\t" + cp.est(task) + "\t" + cp.eft(task));
-        }
-        System.out.println("Critical path: " + cp.getCriticalPathLength());
-	}
-	
-	@Test
-	public void testEft30() {
-		DAG dag = DAGParser.parseDAG(new File("dags/CyberShake_30.dag"));
+        
+        Task A = dag.getTask("A");
+        Task B = dag.getTask("B");
+        Task C = dag.getTask("C");
+        Task D = dag.getTask("D");
+        Task E = dag.getTask("E");
+        
+        assertEquals(0.0, cp.est(A), 0.00001);
+        assertEquals(1.0, cp.eft(A), 0.00001);
+        
+        assertEquals(1.0, cp.est(B), 0.00001);
+        assertEquals(2.0, cp.eft(B), 0.00001);
+        
+        assertEquals(2.0, cp.est(C), 0.00001);
+        assertEquals(3.0, cp.eft(C), 0.00001);
+        
+        assertEquals(3.0, cp.est(D), 0.00001);
+        assertEquals(4.0, cp.eft(D), 0.00001);
+        
+        assertEquals(4.0, cp.est(E), 0.00001);
+        assertEquals(5.0, cp.eft(E), 0.00001);
+        
+        assertEquals(5.0, cp.getCriticalPathLength(), 0.00001);
+    }
+    
+    @Test
+    public void test() {
+        DAG dag = DAGParser.parseDAG(new File("dags/test.dag"));
         TopologicalOrder order = new TopologicalOrder(dag);
-        for (Task task : order.order()) System.out.print(task + " "); System.out.println();
         CriticalPath cp = new CriticalPath(dag, order);
-        for (Task task : order.order()) {
-        	System.out.println(task + "\t" + task.size + "\t" + cp.est(task) + "\t" + cp.eft(task));
-        }
-        System.out.println("Critical path: " + cp.getCriticalPathLength());
-	}
-
+        assertEquals(21, cp.getCriticalPathLength(), 0.00001);
+    }
+    
+    @Test
+    public void cybershake30() {
+        DAG dag = DAGParser.parseDAG(new File("dags/CyberShake_30.dag"));
+        TopologicalOrder order = new TopologicalOrder(dag);
+        CriticalPath cp = new CriticalPath(dag, order);
+        assertEquals(221.84, cp.getCriticalPathLength(), 0.00001);
+    }
 }
