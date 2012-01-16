8208713ad3f5d8ac9d3635dd430bcd033ce91332
malawski <malawski@agh.edu.pl>
2012-01-16 16:55:38 -0500

    more plots added

---
+++ scripts/copy_plots_to_paper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+cp MONTAGE.n.1000.8.dagh1-20m0.pdf ../paper/figures/MONTAGE-n-1000-8-dagh1-20m0.pdf
+cp CYBERSHAKE.n.1000.8.dagh1-20m0.pdf ../paper/figures/CYBERSHAKE-n-1000-8-dagh1-20m0.pdf
+cp LIGO.n.1000.8.dagh1-40m0.pdf ../paper/figures/LIGO-n-1000-8-dagh1-40m0.pdf
+cp GENOME.n.1000.8.dagh100-1500m0.pdf ../paper/figures/GENOME-n-1000-8-dagh100-1500m0.pdf
+cp SIPHT.n.1000.8.dagh5-50m0.pdf ../paper/figures/SIPHT-n-1000-8-dagh5-50m0.pdf
+
+#cp score-MONTAGE.n.1000.8.dagh1-20m0.pdf ../paper/figures/score-MONTAGE-n-1000-8-dagh1-20m0.pdf
+#cp score-CYBERSHAKE.n.1000.8.dagh1-20m0.pdf ../paper/figures/score-CYBERSHAKE-n-1000-8-dagh1-20m0.pdf
+#cp score-LIGO.n.1000.8.dagh1-40m0.pdf ../paper/figures/score-LIGO-n-1000-8-dagh1-40m0.pdf
+#cp score-GENOME.n.1000.8.dagh100-1500m0.pdf ../paper/figures/score-GENOME-n-1000-8-dagh100-1500m0.pdf
+#cp score-SIPHT.n.1000.8.dagh5-50m0.pdf ../paper/figures/score-SIPHT-n-1000-8-dagh5-50m0.pdf
+
+cp score-MONTAGE.n.1000.8.dagh1-20m0.pdf ../paper/figures/score2-MONTAGE-n-1000-8-dagh1-20m0.pdf
+cp score-CYBERSHAKE.n.1000.8.dagh1-20m0.pdf ../paper/figures/score2-CYBERSHAKE-n-1000-8-dagh1-20m0.pdf
+cp score-LIGO.n.1000.8.dagh1-40m0.pdf ../paper/figures/score2-LIGO-n-1000-8-dagh1-40m0.pdf
+cp score-GENOME.n.1000.8.dagh100-1500m0.pdf ../paper/figures/score2-GENOME-n-1000-8-dagh100-1500m0.pdf
+cp score-SIPHT.n.1000.8.dagh5-50m0.pdf ../paper/figures/score2-SIPHT-n-1000-8-dagh5-50m0.pdf
+
+
+#cp score-MONTAGE.n.1000.8.dagh1-20m0.pdf ../paper/figures/cost-MONTAGE-n-1000-8-dagh1-20m0.pdf
+#cp score-CYBERSHAKE.n.1000.8.dagh1-20m0.pdf ../paper/figures/cost-CYBERSHAKE-n-1000-8-dagh1-20m0.pdf
+#cp score-LIGO.n.1000.8.dagh1-40m0.pdf ../paper/figures/cost-LIGO-n-1000-8-dagh1-40m0.pdf
+#cp score-GENOME.n.1000.8.dagh100-1500m0.pdf ../paper/figures/cost-GENOME-n-1000-8-dagh100-1500m0.pdf
+#cp score-SIPHT.n.1000.8.dagh5-50m0.pdf ../paper/figures/cost-SIPHT-n-1000-8-dagh5-50m0.pdf
+
+#cp score-MONTAGE.n.1000.8.dagh1-20m0.pdf ../paper/figures/size-MONTAGE-n-1000-8-dagh1-20m0.pdf
+#cp score-CYBERSHAKE.n.1000.8.dagh1-20m0.pdf ../paper/figures/size-CYBERSHAKE-n-1000-8-dagh1-20m0.pdf
+#cp score-LIGO.n.1000.8.dagh1-40m0.pdf ../paper/figures/size-LIGO-n-1000-8-dagh1-40m0.pdf
+#cp score-GENOME.n.1000.8.dagh100-1500m0.pdf ../paper/figures/size-GENOME-n-1000-8-dagh100-1500m0.pdf
+#cp score-SIPHT.n.1000.8.dagh5-50m0.pdf ../paper/figures/size-SIPHT-n-1000-8-dagh5-50m0.pdf
