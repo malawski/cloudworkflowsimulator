366cfe80da1532c7c7d2a9d586447352e5f4fa5a
mequrel <meq@meq.com>
2013-06-16 18:30:34 +0200

    logs processing script - refactoring

--- scripts/mequrel/prepare_data_for_gantt.py
+++ scripts/mequrel/prepare_data_for_gantt.py
@@ -9,14 +9,12 @@ Input format:
 1.3055099544444388 (38.556320549999995) Starting computational part of job ID0012 on VM 13
 0.0 (38.587238909999996)  Job ID00007 finished on VM 12
 0.0 (38.587238909999996)  Starting job 67 on VM 12
-0.0 (38.587238909999996) Global transfer started: chr21.1.2.sfq, size: 37006219
-0.0 (39.05000127555556)  Starting job 47 on VM 24
 (...)
 
 Output format [jobId, VM, inputsStartTime, computationalTaskStartTime, computationalTaskEndTime, outputsEndTime]:
 
-00007 12 31.6 32.5 38.9
-00017 4 29.3 30.1 32.8
+00007 12 31.6 32.5 38.9 40.9
+00017 4 29.3 30.1 32.8 38.9
 
 """
 
@@ -36,7 +34,7 @@ def main():
 
     infile.close()
 
-    printTasks();
+    printTasks()
 
 
 startedTaskPattern = r"\d+.\d+ \((\d+.\d+)\)\s+Starting job ID(\d+) on VM (\d+)"
@@ -44,18 +42,29 @@ startedComputationPattern = r"\d+.\d+ \((\d+.\d+)\)\s+Starting computational par
 finishedComputationPattern = r"\d+.\d+ \((\d+.\d+)\)\s+Computational part of job ID(\d+) on VM (\d+) finished"
 finishedTaskPattern = r"\d+.\d+ \((\d+.\d+)\)\s+Job ID(\d+) finished on VM (\d+)"
 
+class Task:
+    def __init__(self, vm):
+        self.vm = vm
+        self.startTaskTime = -1
+        self.startComputationTime = -1
+        self.finishComputationTime = -1
+        self.finishTaskTime = -1
+
 tasks = {}
 
 def printTasks():
     for id in tasks:
         task = tasks[id]
 
-        print ("{} {} {} {} {} {}".format(id, task[0], task[1], task[2], task[3], task[4]))
+        print ("{} {} {} {} {} {}".format(id, task.vm, task.startTaskTime, task.startComputationTime,
+            task.finishComputationTime, task.finishTaskTime))
 
 
-def createTaskIfNotExists(task):
+def createTaskIfNotExistsOrUpdateVM(task, vm):
     if not task in tasks:
-        tasks[task] = [None, -1, -1, -1, -1]
+        tasks[task] = Task(vm)
+    else:
+        tasks[task].vm = vm
 
 
 def maybeStartedTask(line):
@@ -66,12 +75,8 @@ def maybeStartedTask(line):
         id = match.group(2)
         vm = match.group(3)
 
-        createTaskIfNotExists(id)
-
-        tasks[id][0] = vm
-        tasks[id][1] = time
-
-    return bool(match)
+        createTaskIfNotExistsOrUpdateVM(id, vm)
+        tasks[id].startTaskTime = time
 
 
 def maybeFinishedTask(line):
@@ -82,12 +87,8 @@ def maybeFinishedTask(line):
         id = match.group(2)
         vm = match.group(3)
 
-        createTaskIfNotExists(id)
-
-        tasks[id][0] = vm
-        tasks[id][4] = time
-
-    return bool(match)
+        createTaskIfNotExistsOrUpdateVM(id, vm)
+        tasks[id].finishTaskTime = time
 
 
 def maybeFinishedComputation(line):
@@ -98,12 +99,8 @@ def maybeFinishedComputation(line):
         id = match.group(2)
         vm = match.group(3)
 
-        createTaskIfNotExists(id)
-
-        tasks[id][0] = vm
-        tasks[id][2] = time
-
-    return bool(match)
+        createTaskIfNotExistsOrUpdateVM(id, vm)
+        tasks[id].finishComputationTime = time
 
 
 def maybeStartedComputation(line):
@@ -114,12 +111,8 @@ def maybeStartedComputation(line):
         id = match.group(2)
         vm = match.group(3)
 
-        createTaskIfNotExists(id)
-
-        tasks[id][0] = vm
-        tasks[id][3] = time
-
-    return bool(match)
+        createTaskIfNotExistsOrUpdateVM(id, vm)
+        tasks[id].startComputationTime = time
 
 
 if __name__ == "__main__":
