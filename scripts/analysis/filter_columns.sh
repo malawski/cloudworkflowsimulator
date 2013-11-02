52a1a2b0aedea1770b3db9947aa14c4275f42d1b
bryk <piotrek.bryk@gmail.com>
2013-11-02 16:49:08 +0100

    Improved scripts documentation

--- scripts/analysis/filter_columns.sh
+++ scripts/analysis/filter_columns.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Filters the given CSV file leaving only selected columns.
-# TODO(bryk): Add more comments.
+# Example: you have a file with 10 columns named A,B,C,...,X and you call this script with
+# A,C,F params, the output CSV file will contain only those columns.
 
 if [ "$#" -ne 2 ]; then
   echo -e "Usage:\n\t$0 [column list] [simulation output CSV file]";
@@ -11,4 +12,3 @@ COLS=$1;
 
 echo $COLS;
 csvtool namedcol $COLS $2;
-
