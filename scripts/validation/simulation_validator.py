724cc86313ad05fedc2264759e47ff2c13605a5b
mequrel <meq@meq.com>
2013-11-15 23:58:19 +0100

    Simulation validator - check if all jobs and transfers were executed within VM lifecycle

--- scripts/validation/simulation_validator.py
+++ scripts/validation/simulation_validator.py
@@ -7,6 +7,7 @@ from validation.order_validator import ValidationResult
 
 ENDS, STARTS = range(2)
 
+#TODO(mequrel): optimize collision detection
 
 def generate_job_events_sequentially(jobs):
     events = []
@@ -51,17 +52,59 @@ def group_by_dict(list_to_group, getter):
     return {key: list(sub_list) for key, sub_list in groupby(list_to_group, getter)}
 
 
-def validate(jobs, transfers):
+def is_fully_within(event, vm):
+    return vm.started <= event.started and event.finished <= vm.finished
+
+
+def get_events_out_of_lifecycle(events, vm):
+    return [event for event in events if not is_fully_within(event, vm)]
+
+
+def validate(jobs, transfers, vms):
+    """
+
+    Checks if simulation was done according to reality.
+
+    :param jobs: list
+    :param transfers: list
+    :param vms: list
+    :return: ValidationResult
+    """
     jobs_by_vm = group_by_dict(jobs, attrgetter('vm'))
     transfers_by_vm = group_by_dict(transfers, attrgetter('vm'))
+    vms = {vm.id: vm for vm in vms}
 
     errors = []
 
-    vms = set(jobs_by_vm.keys()) | set(transfers_by_vm.keys())
+    jobs_vm_ids = set(jobs_by_vm.keys()) | set(transfers_by_vm.keys())
 
-    for vm in vms:
-        vm_jobs = jobs_by_vm[vm] if vm in jobs_by_vm else []
-        vm_transfers = transfers_by_vm[vm] if vm in transfers_by_vm else []
+    for vm_id in jobs_vm_ids:
+
+        vm_jobs = jobs_by_vm[vm_id] if vm_id in jobs_by_vm else []
+        vm_transfers = transfers_by_vm[vm_id] if vm_id in transfers_by_vm else []
+
+        if vm_id not in vms:
+            job_errors = ['Job {} was executed on non-existing VM {}'.format(job.id, vm_id)
+                          for job in vm_jobs]
+
+            transfer_errors = ['Transfer {} was executed on non-existing VM {}'.format(transfer.id, vm_id)
+                               for transfer in vm_transfers]
+
+            errors.extend(job_errors)
+            errors.extend(transfer_errors)
+            continue
+
+        vm = vms[vm_id]
+
+        out_of_lifecycle_jobs = get_events_out_of_lifecycle(vm_jobs, vm)
+        job_errors = ['Job {} was executed out of VM {} lifecycle'.format(job.id, vm_id)
+                      for job in out_of_lifecycle_jobs]
+        errors.extend(job_errors)
+
+        out_of_lifecycle_transfers = get_events_out_of_lifecycle(vm_transfers, vm)
+        job_errors = ['Transfer {} was executed out of VM {} lifecycle'.format(transfer.id, vm_id)
+                      for transfer in out_of_lifecycle_transfers]
+        errors.extend(job_errors)
 
         intersecting_jobs = get_intersecting_jobs(vm_jobs)
 
@@ -95,8 +138,9 @@ def main():
 
     jobs = execution_log.events[EventType.TASK]
     transfers = execution_log.events[EventType.TRANSFER]
+    vms = execution_log.events[EventType.VM]
 
-    result = validate(jobs, transfers)
+    result = validate(jobs, transfers, vms)
 
     for error in result.errors:
         print(error)
--- scripts/validation/simulation_validator_test.py
+++ scripts/validation/simulation_validator_test.py
@@ -1,6 +1,7 @@
 import unittest
-from log_parser.execution_log import TaskLog, TransferLog
+from log_parser.execution_log import TaskLog, TransferLog, VMLog
 from validation import simulation_validator
+
 
 def create_job(started, finished, vm):
     return TaskLog(started=started, finished=finished, vm=vm,
@@ -12,13 +13,19 @@ def create_transfer(started, finished, vm):
                        id='123', job_id='before_1', direction='UPLOAD', file_id='transferred.txt')
 
 
+def create_vm(started, finished, id):
+    return VMLog(started=started, finished=finished, id=id)
+
+
 class SimulationValidatorTest(unittest.TestCase):
     def test_should_pass_if_job_does_not_intersects(self):
         jobs = [
             create_job(started=0.0, finished=10.0, vm=1),
             create_job(started=11.0, finished=13.0, vm=1)]
+        vms = [
+            create_vm(started=0.0, finished=13.0, id=1)]
 
-        result = simulation_validator.validate(jobs, [])
+        result = simulation_validator.validate(jobs, [], vms)
 
         self.assertTrue(result.is_valid)
         self.assertEqual([], result.errors)
@@ -27,61 +34,120 @@ class SimulationValidatorTest(unittest.TestCase):
         jobs = [
             create_job(started=0.0, finished=12.0, vm=1),
             create_job(started=11.0, finished=13.0, vm=1)]
+        vms = [
+            create_vm(started=0.0, finished=13.0, id=1)]
 
-        result = simulation_validator.validate(jobs, [])
+        result = simulation_validator.validate(jobs, [], vms)
 
         self.assertFalse(result.is_valid)
-        self.assertEqual(1, len(result.errors))
+        self.assertLessEqual(1, len(result.errors))
 
     def test_should_pass_if_intersecting_jobs_are_on_different_vms(self):
         jobs = [
             create_job(started=0.0, finished=12.0, vm=1),
             create_job(started=11.0, finished=13.0, vm=2)]
+        vms = [
+            create_vm(started=0.0, finished=13.0, id=1),
+            create_vm(started=11.0, finished=13.0, id=2)]
 
-        result = simulation_validator.validate(jobs, [])
+        result = simulation_validator.validate(jobs, [], vms)
 
         self.assertTrue(result.is_valid)
 
     def test_should_pass_if_transfers_not_intersects_computation(self):
         jobs = [
             create_job(started=0.0, finished=5.0, vm=1)]
-
         transfers = [
             create_transfer(started=6.0, finished=12.0, vm=1)]
+        vms = [
+            create_vm(started=0.0, finished=12.0, id=1)]
 
-        result = simulation_validator.validate(jobs, transfers)
+        result = simulation_validator.validate(jobs, transfers, vms)
 
         self.assertTrue(result.is_valid)
 
     def test_should_fail_if_transfer_intersects_computation_on_the_same_vm(self):
         jobs = [
             create_job(started=0.0, finished=5.0, vm=1)]
-
         transfers = [
             create_transfer(started=3.0, finished=9.0, vm=1)]
+        vms = [
+            create_vm(started=0.0, finished=9.0, id=1)]
 
-        result = simulation_validator.validate(jobs, transfers)
+        result = simulation_validator.validate(jobs, transfers, vms)
+
+        self.assertFalse(result.is_valid)
 
     def test_should_pass_if_transfer_intersects_computation_on_different_vms(self):
         jobs = [
             create_job(started=0.0, finished=5.0, vm=1)]
-
         transfers = [
             create_transfer(started=3.0, finished=9.0, vm=2)]
+        vms = [
+            create_vm(started=0.0, finished=10.0, id=1),
+            create_vm(started=0.0, finished=10.0, id=2)]
 
-        result = simulation_validator.validate(jobs, transfers)
+        result = simulation_validator.validate(jobs, transfers, vms)
 
         self.assertTrue(result.is_valid)
 
-    def test_should_pass_if_transfer_intersects_computation_on_different_vms(self):
+    def test_should_pass_if_job_was_done_within_vm_lifecycle(self):
         jobs = [
-            create_job(started=0.0, finished=5.0, vm=1)]
+            create_job(started=3.0, finished=5.0, vm=1)]
+        vms = [
+            create_vm(started=0.0, finished=5.0, id=1)]
 
+        result = simulation_validator.validate(jobs, [], vms)
+
+        self.assertTrue(result.is_valid)
+
+    def test_should_fail_if_job_was_done_on_non_existing_vm(self):
+        jobs = [
+            create_job(started=3.0, finished=5.0, vm=1)]
+        vms = []
+
+        result = simulation_validator.validate(jobs, [], vms)
+
+        self.assertFalse(result.is_valid)
+
+    def test_should_fail_if_job_was_done_out_of_vm_lifecycle(self):
+        jobs = [
+            create_job(started=3.0, finished=5.0, vm=1)]
+        vms = [
+            create_vm(started=0.0, finished=4.0, id=1)]
+
+        result = simulation_validator.validate(jobs, [], vms)
+
+        self.assertFalse(result.is_valid)
+
+    def test_should_pass_if_transfer_was_done_within_vm_lifecycle(self):
         transfers = [
-            create_transfer(started=3.0, finished=9.0, vm=2)]
+            create_transfer(started=3.0, finished=5.0, vm=1)]
+        vms = [
+            create_vm(started=0.0, finished=5.0, id=1)]
 
-        result = simulation_validator.validate(jobs, transfers)
+        result = simulation_validator.validate([], transfers, vms)
 
+        self.assertTrue(result.is_valid)
+
+    def test_should_fail_if_transfer_was_done_on_non_existing_vm(self):
+        transfers = [
+            create_transfer(started=3.0, finished=5.0, vm=1)]
+        vms = []
+
+        result = simulation_validator.validate([], transfers, vms)
+
+        self.assertFalse(result.is_valid)
+
+    def test_should_fail_if_transfer_was_done_out_of_vm_lifecycle(self):
+        transfers = [
+            create_transfer(started=3.0, finished=5.0, vm=1)]
+        vms = [
+            create_vm(started=0.0, finished=4.0, id=1)]
+
+        result = simulation_validator.validate([], transfers, vms)
+
+        self.assertFalse(result.is_valid)
 
 
 if __name__ == '__main__':
