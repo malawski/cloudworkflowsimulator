cc1129197bdf9592b4a7a3683395bcfd1f82c17b
mequrel <meq@meq.com>
2013-11-18 01:46:16 +0100

    Unit tests runner added.

---
+++ scripts/run_unit_tests.sh
@@ -0,0 +1,6 @@
+python -m validation.single_task_validator_test
+python -m validation.order_validator_test
+python -m validation.simulation_validator_test
+python -m validation.constraints_validator_test
+python -m log_parser.parse_experiment_log_test
+
