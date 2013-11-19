4f0fc78b54ea97a315fa97ad38e364f2238b98f7
mequrel <meq@meq.com>
2013-11-20 00:10:33 +0100

    Removed old algorithm.VMType class.

--- src/cws/core/core/VMTypeFactory.java
+++ src/cws/core/core/VMTypeFactory.java
@@ -9,8 +9,4 @@ public class VMTypeFactory {
     public static VMType getDefaults() {
         return new VMTypeBuilder().mips(1000).cores(1).price(1.0).build();
     }
-
-    public static VMType fromOldVMType(cws.core.algorithms.VMType oldVMType) {
-        return new VMTypeBuilder().mips(oldVMType.getMips()).cores(1).price(oldVMType.getPrice()).build();
-    }
 }
