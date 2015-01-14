malawski/cloudworkflowsimulator

commit 7b3c239d72341b5944be6719008a61dd5e66cd76

Handle lists of VMs in Cloud rather than WorkflowEngine
* Removes duplication of lists.
* Improves separation of concerns.

Added getter functions in WorkflowEngine for compatability.

Required adding a real provisioner to some scheduler tests.

src/cws/core/Cloud.java
@@ -2,8 +2,11 @@ package cws.core;
 
 import java.util.HashSet;
 import java.util.Set;
-
 import com.google.common.collect.ImmutableSet;
+
+import java.util.List;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
 
 import cws.core.cloudsim.CWSSimEntity;
 import cws.core.cloudsim.CWSSimEvent;
@@ -13,7 +16,7 @@ import cws.core.exception.UnknownWorkflowEventException;
 /**
  * A Cloud is an entity that handles the provisioning and deprovisioning
  * of VM resources.
- * 
+ *
  * @author Gideon Juve <juve@usc.edu>
  */
 public class Cloud extends CWSSimEntity {
@@ -33,6 +36,26 @@ public class Cloud extends CWSSimEntity {
 
     public Set<VM> getAllVMs() {
         return ImmutableSet.copyOf(vms);
+    }
+
+    public List<VM> getFreeVMs() {
+        Builder<VM> free = ImmutableList.<VM>builder();
+        for (VM vm : vms) {
+            if (!vm.isTerminated() && vm.isFree()) {
+                free.add(vm);
+            }
+        }
+        return free.build();
+    }
+
+    public List<VM> getBusyVMs() {
+        Builder<VM> busy = ImmutableList.<VM>builder();
+        for (VM vm : vms) {
+            if (!vm.isTerminated() && !vm.isFree()) {
+                busy.add(vm);
+            }
+        }
+        return busy.build();
     }
 
     @Override

src/cws/core/WorkflowEngine.java
@@ -2,11 +2,9 @@ package cws.core;
 
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableList.Builder;
 
 import cws.core.cloudsim.CWSSimEntity;
 import cws.core.cloudsim.CWSSimEvent;
@@ -35,9 +33,6 @@ public class WorkflowEngine extends CWSSimEntity {
 
     /** The scheduler that matches jobs to resources for this workflow engine */
     private final Scheduler scheduler;
-
-    /** The currently running VMs */
-    private final LinkedList<VM> availableVms = new LinkedList<VM>();
 
     /** The list of unmatched ready jobs */
     private final LinkedList<Job> queue = new LinkedList<Job>();
@@ -83,7 +78,7 @@ public class WorkflowEngine extends CWSSimEntity {
             break;
         case WorkflowEvent.PROVISIONING_REQUEST:
             if (provisioner != null)
-                if (availableVms.size() > 0 || dags.size() > 0)
+                if (getAvailableVMs().size() > 0 || dags.size() > 0)
                     provisioner.provisionResources(this);
             break;
         default:
@@ -93,7 +88,7 @@ public class WorkflowEngine extends CWSSimEntity {
 
     public double getCost() {
         double ret = cost;
-        for (VM vm : availableVms) {
+        for (VM vm : getAvailableVMs()) {
             ret += vm.getCost();
         }
         return ret;
@@ -107,13 +102,11 @@ public class WorkflowEngine extends CWSSimEntity {
     }
 
     private void vmLaunched(VM vm) {
-        availableVms.add(vm);
         scheduler.scheduleJobs(this);
     }
 
     private void vmTerminated(VM vm) {
         cost += vm.getCost();
-        availableVms.remove(vm);
     }
 
     private void dagSubmit(DAGJob dj) {
@@ -225,33 +218,15 @@ public class WorkflowEngine extends CWSSimEntity {
     }
 
     public List<VM> getAvailableVMs() {
-        Builder<VM> available = ImmutableList.<VM>builder();
-        for (VM vm : availableVms) {
-            if (!vm.isTerminated()) {
-                available.add(vm);
-            }
-        }
-        return available.build();
+        return new ArrayList<>(provisioner.getCloud().getAllVMs());
     }
-    
+
     public List<VM> getFreeVMs() {
-        Builder<VM> free = ImmutableList.<VM>builder();
-        for (VM vm : availableVms) {
-            if (!vm.isTerminated() && vm.isFree()) {
-                free.add(vm);
-            }
-        }
-        return free.build();
+        return provisioner.getCloud().getFreeVMs();
     }
 
     public List<VM> getBusyVMs() {
-        Builder<VM> busy = ImmutableList.<VM>builder();
-        for (VM vm : availableVms) {
-            if (!vm.isTerminated() && !vm.isFree()) {
-                busy.add(vm);
-            }
-        }
-        return busy.build();
+        return provisioner.getCloud().getBusyVMs();
     }
 
     public void addJobListener(JobListener l) {

integration_test/cws/core/scheduler/DAGDynamicSchedulerStorageAwareTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import cws.core.Cloud;
 import cws.core.EnsembleManager;
 import cws.core.Provisioner;
+import cws.core.provisioner.NullProvisioner;
 import cws.core.VM;
 import cws.core.VMFactory;
 import cws.core.WorkflowEngine;
@@ -53,10 +54,11 @@ public class DAGDynamicSchedulerStorageAwareTest {
         vmType = VMTypeBuilder.newBuilder().mips(1).cores(1).price(1.0).build();
         environment = new Environment(vmType, storageManager);
 
-        provisioner = null;
+        provisioner = new NullProvisioner(cloudsim);
         scheduler = new EnsembleDynamicScheduler(cloudsim, environment);
         engine = new WorkflowEngine(provisioner, scheduler, Double.MAX_VALUE, Double.MAX_VALUE, cloudsim);
         cloud = new Cloud(cloudsim);
+        provisioner.setCloud(cloud);
 
         jobLog = new WorkflowLog(cloudsim);
         engine.addJobListener(jobLog);
@@ -66,7 +68,7 @@ public class DAGDynamicSchedulerStorageAwareTest {
     public void shouldTakeIntoConsiderationTransferMakespans() {
         /**
          * input DAG:
-         * 
+         *
          * .....| in.txt 100B = 50s read
          * .....|
          * ...ID000 2s
@@ -76,7 +78,7 @@ public class DAGDynamicSchedulerStorageAwareTest {
          * ...ID001 40s
          * .....|
          * .....| out.txt 200B = 200s write
-         * 
+         *
          */
 
         launchVM();
@@ -85,7 +87,7 @@ public class DAGDynamicSchedulerStorageAwareTest {
 
         /**
          * expected:
-         * 
+         *
          * ... 50.0 Read in.txt
          * .... 2.0 Run job ID000
          * . 1000.0 Write mid.txt

integration_test/cws/core/scheduler/DAGDynamicSchedulerTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import cws.core.Cloud;
 import cws.core.EnsembleManager;
 import cws.core.Provisioner;
+import cws.core.provisioner.NullProvisioner;
 import cws.core.VM;
 import cws.core.VMFactory;
 import cws.core.WorkflowEngine;
@@ -49,10 +50,11 @@ public class DAGDynamicSchedulerTest {
         vmType = VMTypeBuilder.newBuilder().mips(1).cores(1).price(1.0).build();
         environment = new Environment(vmType, storageManager);
 
-        provisioner = null;
+        provisioner = new NullProvisioner(cloudsim);
         scheduler = new EnsembleDynamicScheduler(cloudsim, environment);
         engine = new WorkflowEngine(provisioner, scheduler, Double.MAX_VALUE, Double.MAX_VALUE, cloudsim);
         cloud = new Cloud(cloudsim);
+        provisioner.setCloud(cloud);
 
         jobLog = new WorkflowLog(cloudsim);
         engine.addJobListener(jobLog);

integration_test/cws/core/scheduler/EnsembleDynamicSchedulerTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import cws.core.Cloud;
 import cws.core.EnsembleManager;
 import cws.core.Provisioner;
+import cws.core.provisioner.NullProvisioner;
 import cws.core.VM;
 import cws.core.VMFactory;
 import cws.core.WorkflowEngine;
@@ -49,10 +50,11 @@ public class EnsembleDynamicSchedulerTest {
         vmType = VMTypeBuilder.newBuilder().mips(1).cores(1).price(1.0).build();
         environment = new Environment(vmType, storageManager);
 
-        provisioner = null;
+        provisioner = new NullProvisioner(cloudsim);
         scheduler = new EnsembleDynamicScheduler(cloudsim, environment);
         engine = new WorkflowEngine(provisioner, scheduler, Double.MAX_VALUE, Double.MAX_VALUE, cloudsim);
         cloud = new Cloud(cloudsim);
+        provisioner.setCloud(cloud);
 
         jobLog = new WorkflowLog(cloudsim);
         engine.addJobListener(jobLog);

src/cws/core/Provisioner.java
@@ -19,6 +19,10 @@ public abstract class Provisioner extends CWSSimEntity {
         this.cloud = cloud;
     }
 
+    public Cloud getCloud() {
+        return cloud;
+    }
+
     public void launchVM(int id, VM vm) {
         cloud.launchVM(id, vm);
     }

commit 43324a16da86c395196db33a1d349a0d406adff0

Make Cloud.getAvailableVMs() consistent with old WorkflowEngine
Fixes bug introduced in 7b3c239d72341b5944be6719008a61dd5e66cd76

The List of VMs returned by WorkflowEngine.getAvailableVMs() was
different to that returned by Cloud.getAllVMs() if VM had a
provisioning/deprovisioning delay. Since Cloud.getAllVMs() was used to
implement a new version of WorkflowEngine.getAvailableVMs() this could
change the behaviour of existing code.

Add new function Cloud.getAvailableVMs(), which returns a List identical
to the old implementation of WorkflowEngine.getAvailableVMs(), to fix
this issue.

Remove function Cloud.getAllVMs() because it is never used and is easily
confused with Cloud.getAvailableVMs().

src/cws/core/Cloud.java
@@ -21,8 +21,17 @@ import cws.core.exception.UnknownWorkflowEventException;
  */
 public class Cloud extends CWSSimEntity {
 
-    /** The set of currently active VMs */
-    private final Set<VM> vms = new HashSet<VM>();
+    /** The set of VMs which are available for use (this is different to
+     * _vmsForSanityCheck because it does not include VMs which are waiting
+     * to be launched). This is the collection that was previously returned
+     * by getAllVMs().
+     */
+    private final Set<VM> availableVMs = new HashSet<VM>();
+
+    /** The set of currently active VMs, used for internal sanity checks
+     * only. */
+    private final Set<VM> _vmsForSanityCheck = new HashSet<VM>();
+
 
     private final Set<VMListener> vmListeners = new HashSet<VMListener>();
 
@@ -34,13 +43,13 @@ public class Cloud extends CWSSimEntity {
         vmListeners.add(l);
     }
 
-    public Set<VM> getAllVMs() {
-        return ImmutableSet.copyOf(vms);
+    public List<VM> getAvailableVMs() {
+        return ImmutableList.copyOf(availableVMs);
     }
 
     public List<VM> getFreeVMs() {
         Builder<VM> free = ImmutableList.<VM>builder();
-        for (VM vm : vms) {
+        for (VM vm : availableVMs) {
             if (!vm.isTerminated() && vm.isFree()) {
                 free.add(vm);
             }
@@ -50,7 +59,7 @@ public class Cloud extends CWSSimEntity {
 
     public List<VM> getBusyVMs() {
         Builder<VM> busy = ImmutableList.<VM>builder();
-        for (VM vm : vms) {
+        for (VM vm : availableVMs) {
             if (!vm.isTerminated() && !vm.isFree()) {
                 busy.add(vm);
             }
@@ -85,7 +94,7 @@ public class Cloud extends CWSSimEntity {
         vm.setOwner(owner);
         vm.setCloud(getId());
         vm.setLaunchTime(getCloudsim().clock());
-        vms.add(vm);
+        _vmsForSanityCheck.add(vm);
 
         // We launch the VM now...
         vm.launch();
@@ -109,9 +118,12 @@ public class Cloud extends CWSSimEntity {
 
     private void vmLaunched(VM vm) {
         // Sanity check
-        if (!vms.contains(vm)) {
+        if (!_vmsForSanityCheck.contains(vm)) {
             throw new RuntimeException("Unknown VM");
         }
+
+        // VM is now available
+        availableVMs.add(vm);
 
         // Listeners are informed
         for (VMListener l : vmListeners) {
@@ -127,7 +139,7 @@ public class Cloud extends CWSSimEntity {
      */
     public final void terminateVM(VM vm) {
         // Sanity check
-        if (!vms.contains(vm)) {
+        if (!_vmsForSanityCheck.contains(vm)) {
             throw new RuntimeException("Unknown VM: " + vm.getId());
         }
         // We terminate the VM now...
@@ -135,11 +147,14 @@ public class Cloud extends CWSSimEntity {
 
         // But it isn't gone until after the delay
         getCloudsim().send(getId(), getId(), vm.getDeprovisioningDelay(), WorkflowEvent.VM_TERMINATED, vm);
-        vms.remove(vm);
+        _vmsForSanityCheck.remove(vm);
     }
 
     private void vmTerminated(VM vm) {
         getCloudsim().log(String.format("VM %d terminated", vm.getId()));
+
+        // VM is no longer available
+        availableVMs.remove(vm);
 
         // Listeners find out
         for (VMListener l : vmListeners) {

src/cws/core/WorkflowEngine.java
@@ -214,7 +214,7 @@ public class WorkflowEngine extends CWSSimEntity implements VMListener {
     }
 
     public List<VM> getAvailableVMs() {
-        return new ArrayList<>(provisioner.getCloud().getAllVMs());
+        return new ArrayList<>(provisioner.getCloud().getAvailableVMs());
     }
 
     public List<VM> getFreeVMs() {

test/cws/core/provisioner/NullProvisionerTest.java
@@ -62,7 +62,7 @@ public class NullProvisionerTest {
 
         cloudsim.startSimulation();
 
-        assertThat("All VMs are allocated", cloud.getAllVMs().size(), is(nVMs));
+        assertThat("All VMs are available", cloud.getAvailableVMs().size(), is(nVMs));
     }
 
     @Test
@@ -78,16 +78,65 @@ public class NullProvisionerTest {
                     .build();
 
         final int nVMs = 10;
+        final double startTime = 0.1;
+
         for (int i = 0; i < nVMs; i++) {
             VM vm = VMFactory.createVM(vmType, cloudsim);
-            provisioner.launchVMAtTime(vm, 0.1);
+            provisioner.launchVMAtTime(vm, startTime);
         }
+
+        assertThat("VMs are not yet available",
+                cloud.getAvailableVMs().size(), is(0));
+
 
         cloudsim.startSimulation();
 
-        assertThat("All VMs are allocated", cloud.getAllVMs().size(), is(nVMs));
+        // Check that time has passed
+        assert(cloudsim.clock() > startTime);
 
-        assertThat("VM is allocated at the correct time",
-                cloud.getAllVMs().iterator().next().getLaunchTime(), is(0.1));
+        assertThat("All VMs are now available",
+                cloud.getAvailableVMs().size(), is(nVMs));
+
+        assertThat("VM was launched at the correct time",
+                cloud.getAvailableVMs().iterator().next().getLaunchTime(),
+                is(startTime));
     }
+
+    @Test
+    public void testScheduleVMsWithProvisioningDelay() {
+        Provisioner provisioner = new NullProvisioner(cloudsim);
+        Cloud cloud = new Cloud(cloudsim);
+        provisioner.setCloud(cloud);
+
+        VMType vmType = VMTypeBuilder.newBuilder().mips(1).cores(1)
+                .price(1.0)
+                .provisioningTime(new ConstantDistribution(1.0)) // non-zero
+                .deprovisioningTime(new ConstantDistribution(0.0))
+                .build();
+        VM vm = VMFactory.createVM(vmType, cloudsim);
+        provisioner.launchVM(vm);
+
+
+        assertThat("VM is not yet available",
+                cloud.getAvailableVMs().size(), is(0));
+
+        cloudsim.startSimulation();
+
+        // Check that time has passed
+        assert(cloudsim.clock() > 1.0);
+
+        assertThat("VM is now available",
+                cloud.getAvailableVMs().size(), is(1));
+
+        assertThat("VM was launched at time 0",
+                cloud.getAvailableVMs().iterator().next().getLaunchTime(),
+                is(0.0));
+
+
+        // It would be good if we could insert tests to run at time 0.9 and
+        // 1.1, to check that the VM is provisioned correctly. At the
+        // moment we are only testing that the VM is provisioned sometime
+        // between 0 and infinity!
+    }
+
 }