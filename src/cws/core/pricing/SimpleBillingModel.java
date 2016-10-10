ca060929659185fb66347c482f3bf09bdf801c79
ziaber <marcinziaber8@gmail.com>
2016-10-10 07:05:18 +0200

    initial PricingModel commit

---
+++ src/cws/core/pricing/SimpleBillingModel.java
@@ -0,0 +1,12 @@
+package cws.core.pricing;
+
+/**
+ * Created by Marcin Ziaber on 2016-10-09.
+ */
+public class SimpleBillingModel {
+    /**
+     * For how long we pay in advance
+     */
+    private final double billingTimeInSeconds;
+
+}
