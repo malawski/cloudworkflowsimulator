ee8b5576f4917819ef1273fe75d03ef866cbdac2
pkoniu <patryk.konior@gmail.com>
2016-11-27 14:29:16 +0100

    nicer params passing

--- avg_score_multiple_runs.rb
+++ avg_score_multiple_runs.rb
@@ -1,25 +1,52 @@
+#!/usr/bin/env ruby
+
 require 'roo'
 require 'csv'
+require 'optparse'
 
-SIM_DATE=ARGV[0]
-LOGFILE=ARGV[1]
-RUNS=ARGV[2].to_i
-N_BUDGETS=ARGV[3].to_i
-DEADLINE_COL = ARGV[4].to_i
-BUDGETS_COL = ARGV[5].to_i
-EXP_SCORE_COL = ARGV[6].to_i
-OUTPUT=ARGV[7]
+REQUIRED_OPTIONS=['date', 'logfile', 'runs', 'budgets', 'deadline_col', 'budgets_col', 'exp_score_col', 'output']
+options = {}
+OptionParser.new do |opts|
+  opts.banner = 'Usage: visualize_exp_score.rb [options]'
+
+  opts.on('-i', '--date SIM_DATE', 'Mandatory simulation date') do |v|
+    options['date'] = v
+  end
+  opts.on('-l', '--logfile LOGFILE', 'Mandatory logfile name') do |v|
+    options['logfile'] = v
+  end
+  opts.on('-r', '--runs RUNS', 'Mandatory runs amount') do |v|
+    options['runs'] = v.to_i
+  end
+  opts.on('-n', '--nbudgets N_BUDGETS', 'Mandatory budgets amount') do |v|
+    options['budgets'] = v.to_i
+  end
+  opts.on('-d', '--deadline_col DEADLINE_COL', 'Mandatory deadlines column number') do |v|
+    options['deadline_col'] = v.to_i
+  end
+  opts.on('-b', '--budgets_col BUDGETS_COL', 'Mandatory budgets column number') do |v|
+    options['budgets_col'] = v.to_i
+  end
+  opts.on('-s', '--exp_score_col EXP_SCORE_COL', 'Mandatory score column number') do |v|
+    options['exp_score_col'] = v.to_i
+  end
+  opts.on('-o', '--output OUTPUT', 'Mandatory output file name') do |v|
+    options['output'] = v
+  end
+end.parse!
+
+raise OptionParser::MissingArgument if (REQUIRED_OPTIONS - options.keys).length != 0
 
 csvs = []
-for i in 1..RUNS
-  csvs.push("#{SIM_DATE}/#{i}/csv/#{LOGFILE}_#{i}.csv")
+for i in 1..options['runs']
+  csvs.push("#{options['date']}/#{i}/csv/#{options['logfile']}_#{i}.csv")
 end
 
 each_csv_scores = []
 csvs.each do |csv|
   bla = Roo::CSV.new(csv)
   ble = []
-  bla.column(EXP_SCORE_COL).each do |score|
+  bla.column(options['exp_score_col']).each do |score|
     next if score=="exponential"
     ble.push(score.to_f)
   end
@@ -31,17 +58,17 @@ each_csv_scores_summed = each_csv_scores.transpose.map { |x| x.reduce(:+) }
 each_csv_scores_summed_mean = []
 each_csv_scores_summed.each do |a|
   if a > 0
-    each_csv_scores_summed_mean.push(a/N_BUDGETS)
+    each_csv_scores_summed_mean.push(a/options['budgets'])
   else
     each_csv_scores_summed_mean.push(a)
   end
 end
 
-csv = Roo::CSV.new("#{SIM_DATE}/1/csv/#{LOGFILE}_1.csv")
-deadlines = csv.column(DEADLINE_COL)
-budgets = csv.column(BUDGETS_COL)
+csv = Roo::CSV.new("#{options['date']}/1/csv/#{options['logfile']}_1.csv")
+deadlines = csv.column(options['deadline_col'])
+budgets = csv.column(options['budgets_col'])
 
-CSV.open(OUTPUT, 'wb') do |file|
+CSV.open(options['output'], 'wb') do |file|
   file << ["budget", "deadline", "expontential"]
   each_csv_scores_summed_mean.each_index do |i|
     file << [budgets[i+1], deadlines[i+1], each_csv_scores_summed_mean[i]]
